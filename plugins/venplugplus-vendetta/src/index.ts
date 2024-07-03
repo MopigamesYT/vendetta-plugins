0e7eb09f44d6814ec54dcb9317075bb9d8d7283d
MopigamesYT <mopigames2.0@gmail.com>
2024-07-03 17:00:25 +0200

    fix: Update WebSocket URL validation regex pattern in index.ts

--- plugins/venplugplus-vendetta/src/index.ts
+++ plugins/venplugplus-vendetta/src/index.ts
@@ -1,15 +1,13 @@
 import { logger, commands } from "@vendetta";
-import { findByProps } from "@vendetta/metro"
+import { findByProps } from "@vendetta/metro";
 import Settings from "./Settings";
 import { ButtplugBrowserWebsocketClientConnector, ButtplugClient, ButtplugClientDevice, ButtplugDeviceError } from "buttplug";
 
-const ClydeUtils = findByProps("sendBotMessage")
+const ClydeUtils = findByProps("sendBotMessage");
 let client: ButtplugClient | null = null;
 
-
 export function isValidWebSocketUrl(url: string): boolean {
-    // Regular expression for WebSocket URL validation
-    const webSocketUrlPattern = /^wss?:\/\/[^\s/$.?#].[^\s]*$/;
+    const webSocketUrlPattern = /^wss?:\/\/[^\s\/$.?#].[^\s]*$/;
 
     // Test the URL against the pattern
     return webSocketUrlPattern.test(url);
@@ -19,11 +17,11 @@ export default {
     onLoad: () => {
         logger.log("Hello world! test");
         if (client) {
-            logger.log("aaaaa")
+            logger.log("aaaaa");
         }
     },
     onUnload: () => {
         logger.log("Goodbye, world.");
     },
-    settings: Settings, 
-}
+    settings: Settings,
+};
