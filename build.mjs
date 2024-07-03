54557e338a49787df54f0dd1bb9f0567121bdbd9
Margot <95306417+MopigamesYT@users.noreply.github.com>
2024-07-03 15:04:00 +0200

    Update build.mjs

--- build.mjs
+++ build.mjs
@@ -2,7 +2,7 @@ import { readFile, writeFile, readdir } from "fs/promises";
 import { extname } from "path";
 import { createHash } from "crypto";
 
-import { rollup } from "rollup";
+import { rollup } from "rollup"; 
 import esbuild from "rollup-plugin-esbuild";
 import commonjs from "@rollup/plugin-commonjs";
 import nodeResolve from "@rollup/plugin-node-resolve";
