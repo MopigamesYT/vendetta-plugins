c56409e2867e2fd6439876e356ceaae6a930bcca
MopigamesYT <mopigames2.0@gmail.com>
2024-07-03 18:12:24 +0200

    fix: Update WebSocket URL handling in Settings.tsx

--- plugins/venplugplus-vendetta/src/Settings.tsx
+++ plugins/venplugplus-vendetta/src/Settings.tsx
@@ -19,6 +19,7 @@ export default () => {
         <ScrollView>
             <FormSection>
             <FormInput
+                        label="Websocket URL"
                         type="text"
                         placeholder="Enter Websocket URL"
                         value={storage.url}
